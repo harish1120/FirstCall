6946cdb5a49786c29c60601939a46dd7243ade5d
Harish1120 <harishsunda97@gmail.com>
2026-04-29 18:51:58 -0400

    Updated Deepgram

--- app/database.py
+++ app/database.py
@@ -1,15 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from app.secrets import get_db_password
 
+ENV = os.getenv("APP_ENV", "development")
 
-DATABASE_URL = "sqlite:///./firstcall.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+if ENV == "production":
+    secret_arn = os.getenv("RDS_SECRET_ARN")
+    password = get_db_password(secret_arn)
+    host = os.getenv("RDS_HOST")
+    user = os.getenv("RDS_USER", "postgres")
+    db = os.getenv("RDS_DB", "postgres")
+    DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:5432/{db}"
+    connect_args = {}
+    engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
+else:
+    DATABASE_URL = "sqlite:///./firstcall.db"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(DATABASE_URL, connect_args={
+        "check_same_thread": False}, echo=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
---
+++ app/secrets.py
@@ -0,0 +1,9 @@
+import json
+import boto3
+
+
+def get_db_password(secret_arn: str) -> str:
+    client = boto3.client("secretsmanager", region_name="ca-central-1")
+    response = client.get_secret_value(SecretId=secret_arn)
+    secret = json.loads(response["SecretString"])
+    return secret["password"]
