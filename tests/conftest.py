de08b330c596ab9fe88a068ab52d3f610bdd9d8f
Harish1120 <harishsunda97@gmail.com>
2026-05-01 10:01:47 -0400

    Add tests for protocols and agent

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ.setdefault("OPENAI_API_KEY", "test")
+os.environ.setdefault("DEEPGRAM_API_KEY", "test")
+os.environ.setdefault("ELEVENLABS_API_KEY", "test")
+os.environ.setdefault("TWILIO_AUTH_TOKEN", "test")
+os.environ.setdefault("APP_ENV", "test")
+os.environ.setdefault("BASE_URL", "test")
+os.environ.setdefault("TWILIO_ACCOUNT_SID", "test")
+os.environ.setdefault("TWILIO_PHONE_NUMBER", "test")
